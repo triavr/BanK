55b97107b35aefab7b3d113d50e0589704846ef7
Igor <triavr@mail.ru>
2022-08-16 17:41:54 +0300

    333

--- src/main/java/com/example/bank/service/BankService.java
+++ src/main/java/com/example/bank/service/BankService.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class BankService {
 
-    private final BalanceRepository DDDbalanceRepository;                          //инжектим бин BalanceRepository
+    private final BalanceRepository balanceRepository;                          //инжектим бин BalanceRepository
 
 //    public BankService(BalanceRepository balanceRepository) {                //вместо конструктора ставим аннотацию @AllArgsConstructor от Lombok
 //        this.balanceRepository = balanceRepository;
